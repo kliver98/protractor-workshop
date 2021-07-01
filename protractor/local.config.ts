57093e337a608085ac05d08f6b212fd5315effc4
Kliver Giron Castro <kliver.gironc@perficient.com>
2021-07-01 11:30:07 -0500

    3. Agregando Reporte a la Consola

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,7 +1,11 @@
+import { reporter } from './helpers/reporter';
 import { Config } from 'protractor';
 
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub'
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  onPrepare: () => {
+    reporter();
+  }
 };
