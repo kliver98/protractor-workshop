13fc3984a10c40a7c07535c80012b9268da66fda
Kliver Daniel <36433884+kliver98@users.noreply.github.com>
2021-07-01 15:07:41 -0500

    Module 5: Chrome Headless (#6)
    * 5. Configure Chrome Headless

---
+++ protractor/headless.config.ts
@@ -0,0 +1,17 @@
+import { reporter } from './helpers/reporter';
+import { Config } from 'protractor';
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: [ '../test/google.spec.js' ],
+  SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  },
+  onPrepare: () => {
+    reporter();
+  }
+};
