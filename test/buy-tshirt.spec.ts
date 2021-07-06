90f6009a80510c85a2511c419770eb33ffd84be6
Kliver Daniel Giron Castro <kliver.gironc@perficient.com>
2021-07-06 17:33:11 -0500

    Change load wait of page and Jasmine

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -23,14 +23,10 @@ describe('Buy a t-shirt', () => {
   const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
   const productListPage: ProductListPage = new ProductListPage();
   const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  beforeEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
-  });
 
   it('then should be bought a t-shirt', async () => {
     await browser.waitForAngularEnabled(false);
     await browser.get('http://automationpractice.com/');
-    await(browser.sleep(3000));
     await menuContentPage.goToTShirtMenu();
     await(browser.sleep(3000));
     await productListPage.selectAddToCart();
