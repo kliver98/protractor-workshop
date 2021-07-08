4999ab745e12db6c7c48c436b248ceaf7802203c
Kliver Daniel <36433884+kliver98@users.noreply.github.com>
2021-07-08 10:13:15 -0500

    Added explicit waits (#14)

--- src/page/product-added-modal.page.ts
+++ src/page/product-added-modal.page.ts
@@ -1,4 +1,4 @@
-import { $, ElementFinder } from 'protractor';
+import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
 
 export class ProductAddedModalPage {
   private proceedToCheckout: ElementFinder;
@@ -8,6 +8,7 @@ export class ProductAddedModalPage {
   }
 
   public async goToOrderSummaryStep(): Promise<void> {
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.proceedToCheckout));
     await this.proceedToCheckout.click();
   }
 }
