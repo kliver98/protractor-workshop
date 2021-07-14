b23559f8275311eb14459bb1bef95b752a1fd328
Kliver Daniel <36433884+kliver98@users.noreply.github.com>
2021-07-14 11:55:12 -0500

    Add jasmine awesome (#17)
    * add jasmine awesome

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,4 +1,13 @@
 import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
+
+const { AwesomeReport } = require('jasmine-awesome-report');
+
+const config = {
+  fullPath: 'reports',
+  fileName: 'awesome',
+  merge: true
+};
+
 export let reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
@@ -6,4 +15,7 @@ export let reporter = () => {
       displayDuration: true
     }
   }));
+
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
+
 };
