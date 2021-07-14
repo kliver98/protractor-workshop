6aa573af3f93e79065b40dea00982aa6147df3ae
Kliver Daniel <36433884+kliver98@users.noreply.github.com>
2021-07-14 13:52:12 -0500

    Add List of products and find by name (#19)

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,14 +1,33 @@
-import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
+import { $, $$, ElementFinder, ElementArrayFinder, browser, ExpectedConditions } from 'protractor';
 
 export class ProductListPage {
-  private addToCart: ElementFinder;
+  private addToCartBtn: ElementFinder;
+  private products: ElementArrayFinder;
 
   constructor () {
-    this.addToCart = $('#center_column [title="Add to cart"]');
+    this.addToCartBtn = $('#add_to_cart > button');
+    this.products = $$('.product_list');
+  }
+
+  public async selectProduct(productName: string): Promise<void> {
+    const product = this.findByProduct(productName);
+    await browser.wait(ExpectedConditions.elementToBeClickable(product.$('img')));
+    product.$('img').click();
+  }
+
+  private findByProduct(productName: string): ElementFinder {
+    return this.products
+      .filter(
+        (product: ElementFinder) =>
+          product
+          .$('[itemprop="name"]')
+          .getText()
+          .then((text : string) => text === productName)
+    ).first();
   }
 
   public async selectAddToCart(): Promise<void> {
-    await browser.wait(ExpectedConditions.elementToBeClickable(this.addToCart));
-    await this.addToCart.click();
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.addToCartBtn));
+    await this.addToCartBtn.click();
   }
 }
