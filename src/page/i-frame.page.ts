af051cba645cdee4eea42142a454b68660342d95
Kliver Daniel Giron Castro <kliver.gironc@perficient.com>
2021-07-19 12:24:11 -0500

    Working with IFrames

--- src/page/i-frame.page.ts
+++ src/page/i-frame.page.ts
@@ -2,9 +2,25 @@ import { $, browser, ElementFinder } from 'protractor';
 
 export class IFramePage {
   private iFrame1: ElementFinder;
+  private currentPageTitle: ElementFinder;
 
   constructor() {
     this.iFrame1 = $('#frame1');
+    this.currentPageTitle = $('.main-header');
+  }
+
+  public async getPageTitle(): Promise<string> {
+    return await this.currentPageTitle.getText();
+  }
+
+  public async switchToFrame(): Promise<void> {
+    this.currentPageTitle = $('#sampleHeading');
+    await browser.switchTo().frame(this.iFrame1.getWebElement());
+  }
+
+  public async switchToMainPage(): Promise<void> {
+    this.currentPageTitle = $('.main-header');
+    await browser.switchTo().defaultContent();
   }
 
   public async setFormFrameHeight(height: number): Promise<void> {
--- test/i-frame.spec.ts
+++ test/i-frame.spec.ts
@@ -1,22 +1,47 @@
 import { browser } from 'protractor';
 import { IFramePage } from '../src/page';
 
-describe('Given an IFrame page', () => {
+describe('Given a web-page with a frame', () => {
+  const iFramePage: IFramePage = new IFramePage();
+
   beforeAll(async () => {
     browser.waitForAngularEnabled(false);
     await browser.get('https://demoqa.com/frames');
   });
 
-  describe('When the height is modified', () => {
-    const iFramePage: IFramePage = new IFramePage();
-    const height = 500;
+  it('Then it should have a title', async () => {
+    expect(await iFramePage.getPageTitle()).toEqual('Frames');
+  });
 
+  describe('When switch to frame', () => {
     beforeAll(async () => {
-      await iFramePage.setFormFrameHeight(height);
+      await iFramePage.switchToFrame();
     });
 
-    it('Then it should have a new height', async () => {
-      expect(await iFramePage.getIFrameHeight()).toEqual(height);
+    it('Then it should have a frame title', async () => {
+      expect(await iFramePage.getPageTitle()).toEqual('This is a sample page');
+    });
+
+    describe('When switch to main page', () => {
+      beforeAll(async () => {
+        await iFramePage.switchToMainPage();
+      });
+
+      it('Then it should have a title', async () => {
+        expect(await iFramePage.getPageTitle()).toEqual('Frames');
+      });
+
+      describe('When the height is modified', () => {
+        const height = 500;
+
+        beforeAll(async () => {
+          await iFramePage.setFormFrameHeight(height);
+        });
+
+        it('Then it should have a new height', async () => {
+          expect(await iFramePage.getIFrameHeight()).toEqual(height);
+        });
+      });
     });
   });
 });
